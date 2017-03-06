2df1060345b79640ed1c63b8437e1f944a275a04
方世翀 <fangshichong0210@gmail.com>
2017-03-06 23:24:29 +0000

    no message

--- Assignment3/Battle ball.java
+++ Assignment3/Battle ball.java
@@ -10,3 +10,39 @@ public MoveBallFrame(){
     list.add(mt1);
   }
 }
+// draw a ball
+for (int i =0; i<list.size(); i++){
+  MoveThread mt = list.get(i);
+  g.fillOval(mt.x, mt.y, mt.width, mt.width);
+}
+//Define the ball coordinates and variables
+public int x = 100, y = 100;
+//Defines the diameter of the ball
+public int width  =60;
+
+
+public void checkHit（）{
+  for(int i = 0; i< MoveBallFrame.list.size(),i++) {
+    MoveThread mt1 = MoveBallFrame.list.get(i);
+    if(mt1==this){
+      continue;
+    }
+//When the extracted ball MT1 and the this object collide are compared
+int xMt1 = mt1.x+mt1.width/2;
+int yMt1 = mt1.y+mt1.width/2;
+
+int xThis = this.x + this.width/2;
+int yThis = this.y + this.width/2;
+
+int distance = (xMt1-xThis)*(xMt1-xThis)+(yMt1-yMt1-yThis)*(yMt1-yThis);
+int length = (mt1.width/2+this.width/2)*(mt1.width/2+this.width/2);
+
+//Compare the size
+if(distance<=length){
+  this.xSpeed = -this.xSpeed;
+  this.ySpeed = -this.ySpeed;
+  mt1.xSpeed = -mt1.xSpeed;
+  mt1.ySpeed = -mt1.ySpeed;
+}
+  }
+}
