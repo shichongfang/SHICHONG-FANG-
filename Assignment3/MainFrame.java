670f127f77ea32ac984525b05fac8bcc85aa2621
方世翀 <fangshichong0210@gmail.com>
2017-03-07 22:48:38 +0000

    no message

---
+++ Assignment3/MainFrame.java
@@ -0,0 +1,12 @@
+package com.shichong.fang;
+
+import java.awt.Color;
+
+public class MainFrame {
+
+    public static void main(String[] args) {
+        //Program entry
+        MoveBallGame mbg = new MoveBallGame();
+        mbg.initFrame();
+    }
+}
