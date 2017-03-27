309c554a74854b79d14d3c5e5a2f5d7b2aca4607
方世翀 <fangshichong0210@gmail.com>
2017-03-27 01:59:49 +0100

    no message

---
+++ Assignment3/Tower Defense Project/Assets/Scripts/ViewController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewController : MonoBehaviour {
+
+	public float speed = 28;
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		transform.Translate (new Vector3 (h, 0, v) *Time.deltaTime*speed ,Space.World);
+	}
+}
