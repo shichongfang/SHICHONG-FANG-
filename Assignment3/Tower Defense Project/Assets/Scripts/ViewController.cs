f262279732499accbe8e8eef7c39eb0f87db0df3
方世翀 <fangshichong0210@gmail.com>
2017-03-29 02:16:26 +0100

    no message

--- Assignment3/Tower Defense Project/Assets/Scripts/ViewController.cs
+++ Assignment3/Tower Defense Project/Assets/Scripts/ViewController.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class ViewController : MonoBehaviour {
 
 	public float speed = 28;
-	
+	public float mouseSpeed = 300;
 	// Update is called once per frame
 	void Update () {
 		float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis("Vertical");
-		transform.Translate (new Vector3 (h, 0, v) *Time.deltaTime*speed ,Space.World);
+		float mouse = Input.GetAxis("Mouse ScrollWheel");
+		transform.Translate (new Vector3 (h*speed, mouse*speed, v*speed) *Time.deltaTime*speed ,Space.World);
 	}
 }
