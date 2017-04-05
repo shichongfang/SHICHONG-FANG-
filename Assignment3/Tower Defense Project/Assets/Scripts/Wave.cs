6384e06926639a462e3d7be3b09033fd22d8f223
方世翀 <fangshichong0210@gmail.com>
2017-04-05 01:27:33 +0100

    no message

---
+++ Assignment3/Tower Defense Project/Assets/Scripts/Wave.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Save Every wave of Enemy's attribute needs 
+public class Wave {
+	public GameObject enemyPrefab;
+	public int count;
+	public float rate;
+}
+
