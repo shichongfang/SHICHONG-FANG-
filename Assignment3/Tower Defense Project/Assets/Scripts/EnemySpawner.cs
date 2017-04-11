25a732db2172b77c247fdb02b7647ea92678f39e
方世翀 <fangshichong0210@gmail.com>
2017-04-11 23:11:23 +0100

    no message

--- Assignment3/Tower Defense Project/Assets/Scripts/EnemySpawner.cs
+++ Assignment3/Tower Defense Project/Assets/Scripts/EnemySpawner.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour {
 
 	public Wave[] waves;
-	public int id;
-
-	// Use this for initialization
-	void Start () {
-		
+	public Transform START;
+    public float waveRate = 3;
+	void Start()
+	{
+		StartCoroutine(SpawnEnemy());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	IEnumerator SpawnEnemy()
+	{
+		foreach (Wave wave in waves) 
+		{
+			for(int i = 0; i < wave.count; i++)
+			{
+				GameObject.Instantiate(wave.enemyPrefab, START.position, Quaternion.identity);
+				if(i !=wave.count-1)
+				   yield return new WaitForSeconds(wave.rate);
+			}
+			yield return new WaitForSeconds(waveRate);
+		}
 	}
 }
