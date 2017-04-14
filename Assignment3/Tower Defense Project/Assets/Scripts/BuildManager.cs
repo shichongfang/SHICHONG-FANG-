53c6b553ec05c1d8ff6cafdb6e60d35f85f17255
方世翀 <fangshichong0210@gmail.com>
2017-04-14 16:12:21 +0100

    no message

--- Assignment3/Tower Defense Project/Assets/Scripts/BuildManager.cs
+++ Assignment3/Tower Defense Project/Assets/Scripts/BuildManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class BuildManager : MonoBehaviour {
 
@@ -8,32 +10,79 @@ public class BuildManager : MonoBehaviour {
 	public TurretData missileTurretData;
 	public TurretData standardTurretData;
 
-	//Indicates the current turret to be built
-	public TurretData selectedTurretData;
+	//表示当前选择的炮台(要建造的炮台)
+	private TurretData selectedTurretData;
+
+	public Text moneyText;
+
+	public Animator moneyAnimator;
+
+	private int money = 1000;
+
+	void ChangeMoney(int change=0)
+	{
+		money += change;
+		moneyText.text = "$" + money;
+	}
+	//表示当前选择的炮台(场景中的游戏物体)
+
+	void Update()
+	{
+		if ( Input.GetMouseButtonDown(0))
+		{
+			if (EventSystem.current.IsPointerOverGameObject()==false)
+			{
+				//开发炮台的建造
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				bool isCollider = Physics.Raycast(ray,out hit, 1000, LayerMask.GetMask("MapCube"));
+				if (isCollider)
+				{
+					MapCube mapCube = hit.collider.GetComponent<MapCube>();
+					if (mapCube.turretGo == null)
+					{
+						//can create
+						if(money > selectedTurretData.cost)
+						{
+							ChangeMoney(-selectedTurretData.cost);
+							mapCube.BuildTurret(selectedTurretData.turretPrefab);
+						}
+						else
+						{
+							// not enough money
+							moneyAnimator.SetTrigger("Flicker");
+						}
+					}
+					else
+					{
+						//todo Update
+					}
+
+				}
+			}
+		}
+	}
 
 	public void OnLaserSelected(bool isOn)
 	{
-		if(isOn)
+		if (isOn)
 		{
-		 selectedTurretData = laserTurretData;
+			selectedTurretData = laserTurretData;
 		}
-
 	}
 
 	public void OnMissileSelected(bool isOn)
 	{
-		if(isOn)
+		if (isOn)
 		{
-		 selectedTurretData = missileTurretData;
+			selectedTurretData = missileTurretData;
 		}
-
 	}
-
 	public void OnStandardSelected(bool isOn)
 	{
-		if(isOn)
+		if (isOn)
 		{
-		 selectedTurretData = standardTurretData;
+			selectedTurretData = standardTurretData;
 		}
 	}
 }
---
+++ Assignment3/Tower Defense Project/Assets/Scripts/MapCube.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCube : MonoBehaviour {
+  [HideInInspector]
+	public GameObject turretGo;//save curret turret on the cube
+
+  public void BuildTurret(GameObject turretPrefab)
+	{
+		turretGo = GameObject.Instantiate(turretPrefab,transform.position,Quaternion.identity);
+	}
+}
