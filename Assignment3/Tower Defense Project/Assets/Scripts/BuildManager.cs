8da5b164f13a2a665f919c5e90cb5704709e2cab
方世翀 <fangshichong0210@gmail.com>
2017-04-14 13:46:17 +0100

    no message

--- Assignment3/Tower Defense Project/Assets/Scripts/BuildManager.cs
+++ Assignment3/Tower Defense Project/Assets/Scripts/BuildManager.cs
@@ -7,4 +7,33 @@ public class BuildManager : MonoBehaviour {
 	public TurretData laserTurretData;
 	public TurretData missileTurretData;
 	public TurretData standardTurretData;
+
+	//Indicates the current turret to be built
+	public TurretData selectedTurretData;
+
+	public void OnLaserSelected(bool isOn)
+	{
+		if(isOn)
+		{
+		 selectedTurretData = laserTurretData;
+		}
+
+	}
+
+	public void OnMissileSelected(bool isOn)
+	{
+		if(isOn)
+		{
+		 selectedTurretData = missileTurretData;
+		}
+
+	}
+
+	public void OnStandardSelected(bool isOn)
+	{
+		if(isOn)
+		{
+		 selectedTurretData = standardTurretData;
+		}
+	}
 }
